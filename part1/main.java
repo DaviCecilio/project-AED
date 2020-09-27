04b7ffcb87484cd487d246cf64c046090c60bfb5
Davi Cecílio <davim.cecilio@gmail.com>
2020-09-26 23:21:40 -0300

    refactor(main): add correct function

--- part1/main.java
+++ part1/main.java
@@ -1,76 +1,163 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 #include <math.h>
 
 void clrscr ();
 int menu ();
-void InsertEmployee ();
-void GetAllEmployee ();
-void GetEmployeeById ();
-void DeleteEmployeeById ();
 
-struct Employee_Data {
+struct employee_Data {
   int id;
-  char name[125];
+  char *name;
   float salary;
-  struct Employee_Data *Next;
-  struct Employee_Data *Prev;
+  struct employee_Data *next;
+  struct employee_Data *prev;
+};
+
+typedef struct employee_Data Employee_Data;
+
+Employee_Data *initialize(void)
+{
+    return NULL;
 }
 
-struct Employee_Data *First;
-struct Employee_Data *Last;
+    Employee_Data *InsertEmployeeInList(Employee_Data *l, int newId, char *newName, float newSalary) {
+        
+        Employee_Data *newEmployee = (Employee_Data *)malloc(sizeof(Employee_Data));
+        
+        newEmployee->id = newId;
+        newEmployee->name = newName;
+        newEmployee->salary = newSalary;
+        
+        newEmployee->prev = NULL;
+        newEmployee->next = l;
+        if (l != NULL)
+            l->prev = newEmployee;
 
-int main ()
-{
-  setlocale (LC_ALL, "Portuguese");
-  int menuSelected;
-  clrscr ();
-  printf ("LISTA DUPLAMENTE ENCADEADA ");
-
-  do
-    {
-      menuSelected = menu ();
-      switch (menuSelected)	{
-	case 0:
-	  break;
-	case 1:
-	  clrscr ();
-	  InsertEmployee ();
-	  break;
-	case 2:
-	  clrscr ();
-	  DeleteEmployeeById ();
-	  break;
-	case 3:
-	  clrscr ();
-	  GetEmployeeById ();
-	  break;
-	case 4:
-	  clrscr ();
-	  GetAllEmployee ();
-	  break;
-	default:
-	  clrscr ();
-	  printf ("----- OPCAO INVALIDA -----\n\n");
-	  printf ("----- TENTE NOVAMENTE!!! -----\n\n");
-	  break;
-	}
+        return newEmployee;
     }
-  while (menuSelected != 0);
-  return 0;
-
+    
+    Employee_Data *InsertEmployee(Employee_Data *l)
+    {
+        struct employee_Data *aux;
+        
+        int lastId;
+        
+        for(aux=l; aux!= NULL; aux = aux->next) {
+            if (aux->next == NULL){
+                lastId = aux->id;
+            }
+        }
+        
+        int newId = lastId + 1;
+        char newName[125];
+        float newSalary;
+        
+        printf ("\n\n -- CADASTRO DE FUNCIONARIO -- \n\n"); 
+        
+        printf("\n Digite o nome do funcionário: ");
+        scanf("%s", newName);
+        
+        printf("\n Digite o salario do funcionário: ");
+        scanf("%f", &newSalary);
+        
+    
+    return InsertEmployeeInList(l, newId, newName, newSalary);
 }
 
-void
-clrscr ()
+void GetAllEmployee (Employee_Data *l) {
+
+  Employee_Data *aux; 
+
+  printf ("\n\n -- LISTAGEM DE FUNCIONARIOS --");
+
+  for(aux=l; aux!= NULL; aux = aux->next)	
+    {
+      printf ("\n\n ---------------------");
+      printf ("\n\n Matricula: %d", aux->id);
+      printf ("\n Nome: %s", aux->name);
+      printf ("\n Salario: %f", aux->salary);
+      printf ("\n\n---------------------");
+    }
+}
+
+void GetEmployeeById(Employee_Data *l) {
+    
+    Employee_Data *aux; 
+    int selectedId;
+    int flag = 0;
+
+  printf ("\n\n -- PESQUISA DE FUNCIONARIOS --");
+  
+  printf ("\n\n Digite um id: ");
+  scanf("%d", &selectedId);
+  
+  
+   for(aux=l; aux!= NULL; aux = aux->next)	{
+      if(selectedId == aux->id) {
+          
+       flag = 1;
+       
+       printf ("\n\n Encontramos esse resultado para a pesquisa: ");
+       
+	   printf ("\n\n ---------------------");
+	   
+	   printf ("\n\n Matricula: %d", aux->id);
+	   printf ("\n Nome: %s", aux->name);
+	   printf ("\n Salario: %f", aux->salary);
+	   
+	   printf ("\n\n---------------------");
+      }
+    }
+
+  if (flag == 0)
+    printf ("\n\n Nao encontramos nenhum funcionario com o id: %d ",
+	    selectedId);
+}
+
+Employee_Data* DeleteEmployeeById (Employee_Data *l)
 {
+  Employee_Data *aux;
+  int flag = 0;
+  int selectedId;
+  
+  printf ("\n\n -- REMOVER FUNCIONARIO --");
+  
+  printf ("\n\n Digite um id: ");
+  scanf("%d", &selectedId);
+
+  for(aux=l; aux!= NULL; aux = aux->next)
+    {
+      if (aux->id == selectedId) {
+
+	  printf ("\n\n O funcionario, %d, foi REMOVIDO! \n", aux->id);
+
+	  aux->prev->next = aux->next;
+	  aux->next->prev = aux->prev;
+
+	  aux = NULL;
+	  flag = 1;
+
+	  } else {
+	  l = aux->next;
+	    }
+    }
+
+  free (aux);
+  if (flag == 0)
+    {
+      printf ("Funcionario %d nao existe!\n", selectedId);
+    }
+    return l;
+}
+
+
+void clrscr () {
   system ("cls||clear");
 }
 
-int
-menu ()
-{
+int menu () {
   int menuSelected = 0;
 
   printf ("\n\n ------ MENU DE OPCOES ------ \n");
@@ -88,109 +175,48 @@ menu ()
 }
 
 
-void InsertEmployee (int newId, char newName, float newSalary) {
-
-  struct Employee_Data *aux;
-
-  aux = (struct Employee_Data *) malloc (sizeof (struct Employee_Data));
-
-  aux->id = newId;
-  aux->name = newName;
-  aux->salary = newSalary;
-
-  Last->Next = aux;
-  aux->Prev = Last;
-
-  Last = Last->Next;
-  aux->Next = NULL;
-}
-
-void GetAllEmployee ()
+int main ()
 {
+  setlocale (LC_ALL, "Portuguese");
+  int menuSelected;
+  clrscr ();
+  Employee_Data *l;
+  l = initialize();
+  printf ("LISTA DUPLAMENTE ENCADEADA ");
 
-  struct Employee_Data *aux;
-
-  aux = First->Next;
-
-  printf ("\n\n -- LISTAGEM DE FUNCIONARIOS --");
-
-  while (aux !== NULL)
+  do
     {
-      printf ("\n\n ---------------------");
-      printf ("\n\n Matricula: %d", aux->id);
-      printf ("\n Nome: %s", aux->name);
-      printf ("\n Salario: %f", aux->salary);
-      printf ("\n\n---------------------");
-
-      aux = aux->Next;
-    }
-}
-
-void GetEmployeeById (int selectedId)
-{
-
-  struct Employee_Data *aux;
-
-  int flag = 0;
-
-  aux = First->Next;
-
-  printf ("\n\n -- PESQUISA DE FUNCIONARIOS --");
-
-  while (aux !== NULL)
-    {
-
-      if (aux->id == selectedId)
-	{
-	  printf ("\n\n Encontramos esse resultado para a pesquisa: ");
-	  printf ("\n\n ---------------------");
-	  printf ("\n\n Matricula: %d", aux->id);
-	  printf ("\n Nome: %s", aux->name);
-	  printf ("\n Salario: %f", aux->salary);
-	  printf ("\n\n---------------------");
-
-	  flag = 1;
-	}
-      else
-	aux = aux->Next;
-    }
-  if (flag == 0)
-    printf ("\n\n NC#o encontramos nenhum funcionario com o id: %d ",
-	    selectedId);
-}
-
-
-void DeleteEmployeeById (int selectedId)
-{
-  struct Employee_Data *aux;
-  int flag = 0;
-
-  aux = First->Next;
-
-  while (aux != NULL)
-    {
-      if (aux->id == selectedId)
-	{
-
-	  printf ("\n\n O funcionario, %s, foi REMOVIDO! \n", aux->name);
-
-	  aux->Prev->Next = aux->Next;
-	  aux->Next->Prev = aux->Prev;
-
-	  aux = NULL;
-	  flag = 1;
-
-	}
-      else
-	{
-	  aux = aux->Prox;
-
+      menuSelected = menu ();
+      switch (menuSelected)	{
+	case 0:
+	  break;
+	case 1:
+	  clrscr ();
+	  l = InsertEmployee(l);
+	  break;
+	case 2:
+	  clrscr ();
+	  l = DeleteEmployeeById(l);
+	  break;
+	case 3:
+	  clrscr ();
+	  GetEmployeeById(l);
+	  break;
+	case 4:
+	  clrscr ();
+	  GetAllEmployee(l);
+	  break;
+	default:
+	  clrscr ();
+	  printf ("----- OPCAO INVALIDA -----\n\n");
+	  printf ("----- TENTE NOVAMENTE!!! -----\n\n");
+	  break;
 	}
     }
+  while (menuSelected != 0);
+  return 0;
 
-  free (aux);
-  if (flag == 0)
-    {
-      printf ("Funcionario %d nao existe!\n", selectedId);
-    }
 }
+
+
+
